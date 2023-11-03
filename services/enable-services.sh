cb7b84d654b34dc005904b3f4e01b694a27b8a2e
Sawors <yt.sawors@gmail.com>
2023-11-03 17:13:37 +0100

    added better systemd service support

---
+++ services/enable-services.sh
@@ -0,0 +1,8 @@
+detectService="desktops-detect.service"
+applyService="desktops-apply.service"
+
+sudo ln -s $(pwd)/$detectService /etc/systemd/system
+sudo ln -s $(pwd)/$applyService /etc/systemd/user
+
+sudo systemctl enable $detectService
+systemctl --user enable $applyService
