42ba30576ffb65a0f8220ff026d675584a097e69
Sawors <yt.sawors@gmail.com>
2024-03-16 18:35:02 +0100

    merged partial fetch and full fetch

--- services/enable-services.sh
+++ services/enable-services.sh
@@ -3,7 +3,7 @@ detectService="desktops-detect.service"
 applyService="desktops-apply.service"
 
 sudo ln -s $(pwd)/$detectService /etc/systemd/system
-#sudo ln -s $(pwd)/$applyService /etc/systemd/user
+sudo ln -s $(pwd)/$applyService /etc/systemd/user
 
 sudo systemctl enable $detectService
-#systemctl --user enable $applyService
+systemctl --user enable $applyService
