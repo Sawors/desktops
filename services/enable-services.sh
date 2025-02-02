b5090c96f7486021f8de7a0d39393aadbe7462f0
Sawors <sawors@proton.me>
2025-02-02 16:44:47 +0100

    changed a bit the service activation

--- services/enable-services.sh
+++ services/enable-services.sh
@@ -2,8 +2,10 @@
 detectService="desktops-detect.service"
 applyService="desktops-apply.service"
 
-sudo ln -s $(pwd)/$detectService /etc/systemd/system
-sudo ln -s $(pwd)/$applyService /etc/systemd/user
+desktops_root=/etc/desktops
+
+sudo ln -s $desktops_root/$detectService /etc/systemd/system
+sudo ln -s $desktops_root/$applyService /etc/systemd/user
 
 sudo systemctl enable $detectService
 systemctl --user enable $applyService
